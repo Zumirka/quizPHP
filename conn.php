cafc4214df4ed45206b1cd6fa35ebdb5e515198e
Zumirka <dominika.bartek1207@gmail.com>
2017-11-26 20:01:22 +0100

    change Json

--- conn.php
+++ conn.php
@@ -4,7 +4,7 @@ $mysql_username = "b4d6043aa104f8";
 $mysql_password = "69463135";
 $server_name = "eu-cdbr-west-01.cleardb.com";
 $conn = mysql_connect($server_name, $mysql_username, $mysql_password);
-$selectdb = mysql_select_db($dba_name,$conn);  
+$selectdb = mysqli_select_db($dba_name,$conn);  
 
 
 ?>
