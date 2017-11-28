086fb361befa745a1880034a6999b8d5a2f2d39c
Zumirka <dominika.bartek1207@gmail.com>
2017-11-28 20:03:20 +0100

    z

--- registred.php
+++ registred.php
@@ -1,7 +1,7 @@
 <?php
 require "conn.php";
-$user_name = $_POST["Username"];
-$user_pass = $_POST["Password"];
+$user_name =  $_POST["login"];
+$user_pass = $_POST["password"];
 $mysql_qry = "insert into users (Login, Password) values ('$user_name','$user_pass')"; 
 if($conn->query($mysql_qry) === TRUE)
 {
