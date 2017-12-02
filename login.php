f656db0f53b48175540cd800c77fe68480bdcc54
Zumirka <dominika.bartek1207@gmail.com>
2017-12-02 20:50:34 +0100

    Rename database

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 require "conn.php";
 $user_name = $_POST["login"];
 $user_pass = $_POST["password"];
-$mysql_qry = "select * from users where Login like '$user_name' and Password like '$user_pass';"; 
+$mysql_qry = "select * from user where Login like '$user_name' and Password like '$user_pass';"; 
 $result = mysqli_query( $conn,$mysql_qry); 
 if(mysqli_num_rows($result) > 0)
 {
