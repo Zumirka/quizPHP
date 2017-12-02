064ee41aeef2cc266af0e7e563126f400533021d
Zumirka <dominika.bartek1207@gmail.com>
2017-12-02 20:52:41 +0100

    Rename row in tables

--- registred.php
+++ registred.php
@@ -2,7 +2,7 @@
 require "conn.php";
 $user_name =  $_POST["login"];
 $user_pass = $_POST["password"];
-$mysql_qry = "insert into user (Login, Password) values ('$user_name','$user_pass')"; 
+$mysql_qry = "insert into user (Name, Password) values ('$user_name','$user_pass')"; 
 if($conn->query($mysql_qry) === TRUE)
 {
 	echo  "Insert Successful " ;
