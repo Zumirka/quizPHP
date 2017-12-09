3ddd0722e90c54cd782f29fe1bad74dd83001d32
Zumirka <dominika.bartek1207@gmail.com>
2017-12-09 19:45:22 +0100

    Ukrycie loginów,haseł

--- conn.php
+++ conn.php
@@ -1,7 +1,8 @@
 <?php
-$dba_name = "heroku_94287781fba39f4";
-$mysql_username = "b4d6043aa104f8";
-$mysql_password = "69463135";
-$server_name = "eu-cdbr-west-01.cleardb.com";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$dba_name = substr($url["path"], 1);
+$mysql_username = $url["user"];
+$mysql_password = $url["pass"];
+$server_name = $url["host"];
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $dba_name); 
 ?>
