82e68eadc94445d8f7c4d442e655e730af21ffa7
Zumirka <dominika.bartek1207@gmail.com>
2017-12-28 18:00:09 +0100

    zmiana kodowania już poprawna

--- conn.php
+++ conn.php
@@ -5,4 +5,5 @@ $mysql_username = $url["user"];
 $mysql_password = $url["pass"];
 $server_name = $url["host"];
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $dba_name); 
+$conn->set_charset("utf8");
 ?>
