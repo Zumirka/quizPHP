7d3823dfeb49caec2c01980f6828bdfba3ac0f6b
Zumirka <dominika.bartek1207@gmail.com>
2017-12-28 19:38:09 +0100

    fix bug

--- getTest.php
+++ getTest.php
@@ -4,6 +4,8 @@ $id = 21;
 $diff = 1;
 $mysql_qry = "SELECT q.Difficult,q.Content as Question,a.Content as Answer,a.IsTrue FROM (question q inner join answers a on a.QuestionId=q.Id) INNER JOIN(  
 SELECT q.Id FROM question q where q.CategoryId='$id' and q.Difficult='$diff' order by rand() limit 5) as q2 ON q2.Id=q.Id;"; 
+$result=$conn->query($mysql_qry);
+
 if($result)
 {
 	while($row=mysqli_fetch_array($result))
