836bc7bed6f65394a7ebdaed33bc951a689be20d
Zumirka <dominika.bartek1207@gmail.com>
2017-12-28 17:21:21 +0100

    asd

--- conn.php
+++ conn.php
@@ -5,5 +5,4 @@ $mysql_username = $url["user"];
 $mysql_password = $url["pass"];
 $server_name = $url["host"];
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $dba_name); 
-mysqli_set_charset($con, "utf8");
 ?>
