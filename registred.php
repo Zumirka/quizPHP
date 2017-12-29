edb45d245c83d27e6b412684dcd9de1120412370
Zumirka <dominika.bartek1207@gmail.com>
2017-12-29 14:22:02 +0100

    Update comunicat

--- registred.php
+++ registred.php
@@ -5,12 +5,12 @@ $user_pass = $_POST["password"];
 $mysql_qry = "insert into user (Name, Password) values ('$user_name','$user_pass')"; 
 if($conn->query($mysql_qry) === TRUE)
 {
-	echo  "Insert Successful " ;
+	echo  "Rejestracja pomyślna. Możesz sie zalogować " ;
 	
 }
 else 
 {
-	echo  "Error: ".$mysql_qury . "<br>".$conn->error; 
+	echo  "Błąd. Taki użytkownik już istnieje. \n Wpisz inny login"; 
 }
 $conn->close();
 ?>
