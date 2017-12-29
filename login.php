d27676f7eca504d6250529dff168abd850e86021
Zumirka <dominika.bartek1207@gmail.com>
2017-12-29 16:01:37 +0100

    test

--- login.php
+++ login.php
@@ -1,17 +1,23 @@
 <?php
 require "conn.php";
-$user_name = $_POST['q'];
-$user_pass = $_POST['q'];
-$mysql_qry = "select * from user where Name like q and Password like q;"; 
+$user_name = $_POST["login"];
+$user_pass = $_POST["password"];
+$mysql_qry = "select * from user where Name like '$user_name' and Password like '$user_pass';"; 
 $result = mysqli_query( $conn,$mysql_qry); 
-if(mysqli_num_rows($result) > 0)
-{
-	$row=mysqli_fetch_assoc($result);
-	$name=$row["Name"];
-	echo " \n Logowanie powiodło się. Witaj ".$name ;
+#if(mysqli_num_rows($result) > 0)
+#{
+#	$row=mysqli_fetch_assoc($result);
+#	$name=$row["Name"];
+/*	echo " \n Logowanie powiodło się. Witaj ".$name ;
 }
 else 
 {
 	echo " \n Logowanie nie powiodło się.";
+}*/
+if($result)
+{
+	print(json_encode($name);
+	
 }
+
 ?>
