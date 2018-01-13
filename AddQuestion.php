f8842c3cb8529e898245ff4e357185fdfb4dfaeb
Zumirka <dominika.bartek1207@gmail.com>
2018-01-13 18:09:06 +0100

    Added addQuestionphp

--- AddQuestion.php
+++ AddQuestion.php
@@ -1,55 +1,30 @@
 <?php
 require "conn.php";
+$id_cat =  $_POST["idCategory"];
+$diff = $_POST["diff"];
+$question =  $_POST["question"];
+$answ1 = $_POST["answ1"];
+$answ2 =  $_POST["answ2"];
+$answ3 = $_POST["answ3"];
 
-if(isset($_POST['id_kat']))
-		$id_kat=$_POST['id_kat'];
-	else
-		header("Location: question.php?err=no_id_kat&id=".$id_kat);
-	if(isset($_POST['odp1']))
-		$odp1=$_POST['odp1'];
-	else
-		header("Location: question.php?err=no_odp_1&id=".$id_kat);
-	if(isset($_POST['odp2']))
-		$odp2=$_POST['odp2'];
-	else
-		header("Location: question.php?err=no_odp_2&id=".$id_kat);
-	if(isset($_POST['odp3']))
-		$odp3=$_POST['odp3'];
-	else
-		header("Location: question.php?err=no_odp_3&id=".$id_kat);
-	if(isset($_POST['popr1']))
-		$popr1=$_POST['popr1'];
-		else
-		header("Location: question.php?err=no_popr1&id=".$id_kat);
-	if(isset($_POST['popr2']))
-		$popr2=$_POST['popr2'];
-		else
-		header("Location: question.php?err=no_popr2&id=".$id_kat);
-	if(isset($_POST['popr3']))
-		$popr3=$_POST['popr3'];
-		else
-		header("Location: question.php?err=no_popr3");
-	if(isset($_POST['pkt']))
-		$pkt=$_POST['pkt'];
-		else
-		header("Location: question.php?err=no_pkt&id=".$id_kat);
-	if(isset($_POST['pyt']))
-		$pyt=$_POST['pyt'];
-		else
-		header("Location: question.php?err=no_pyt&id=".$id_kat);
 	
-	
-	$mysql_qry = "insert into question values('','$id_kat','$pyt','$pkt')";
+$mysql_qry = "insert into question (CategoryId,Content,Difficult) values ('$id_cat','$question','$diff')";
 	$result = mysqli_query( $conn,$mysql_qry);
 	// Pobiera id ostatnio dodanego pytania
 	$id_quest=mysqli_insert_id($conn);
-	
-	
-	$mysql_qry = "insert into answears values('','$id_quest','$odp1','$popr1')";
+$mysql_qry = "insert into answears (QuestionId,Content,IsTrue) values('$id_quest','$answ1',1)";
 	$result = mysqli_query( $conn,$mysql_qry);
-	$mysql_qry = "insert into answears values('','$id_quest','$odp2','$popr2')";
+$mysql_qry = "insert into answears (QuestionId,Content,IsTrue) values('$id_quest','$answ2',0)";
 	$result = mysqli_query( $conn,$mysql_qry);
-	$mysql_qry = "insert into answears values('','$id_quest','$odp3','$popr3')";
+$mysql_qry = "insert into answears (QuestionId,Content,IsTrue) values('$id_quest','$answ3',0)";
 	$result = mysqli_query( $conn,$mysql_qry);
+	if($conn->query($mysql_qry) === TRUE)
+	{
+		echo "Dodano pytanie";
+	}
+	else
+	{
+		echo " nie dodano";
+	}
 $conn->close();
 ?>
