0879cfdd01f87a3170615415c38858720c355fe1
Zumirka <dominika.bartek1207@gmail.com>
2018-01-14 18:51:19 +0100

    tests

--- addStatistic.php
+++ addStatistic.php
@@ -10,7 +10,7 @@ $user_id=$conn->query($mysql_qry);
 $mysql_qry = "insert into statistic (UserId,CategoryId,Difficulty,Time,Points,Date) values ('$user_id','$id_cat','$diff','$time','$points',now())";
 	$result = mysqli_query( $conn,$mysql_qry);
 
-	error_log(json_last_error());
+ echo mysqli_error($conn);
 
 $conn->close();
 ?>
