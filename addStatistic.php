9a930e5b3050e2f0ba291c8606af74034216e4ed
Zumirka <dominika.bartek1207@gmail.com>
2018-01-14 18:19:56 +0100

    addStatistic.php

---
+++ addStatistic.php
@@ -0,0 +1,15 @@
+<?php
+require "conn.php";
+$id_user =  $_POST["UserId"];
+$_id_cat = $_POST["CategoryId"];
+$diff =  $_POST["Difficulty"];
+$time = $_POST["Time"];
+$points =  $_POST["Points"];
+$date = $_POST["Date"];
+
+	
+$mysql_qry = "insert into statistic (UserId,CategoryId,Difficulty,Time,Points,Date) values ('$id_user','$id_cat','$diff','$time','$points','$date')";
+	$result = mysqli_query( $conn,$mysql_qry);
+
+$conn->close();
+?>
