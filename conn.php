6b8b69ef019af8e1a5515821c2ae91e1e19ce549
Zumirka <dominika.bartek1207@gmail.com>
2018-01-16 20:46:34 +0100

    time zone

--- conn.php
+++ conn.php
@@ -5,7 +5,6 @@ $mysql_username = $url["user"];
 $mysql_password = $url["pass"];
 $server_name = $url["host"];
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $dba_name); 
-$mysql_qry = "set time_zone='Europe/Warsaw';"; 
-$conn->query($mysql_qry);
+$conn->time_zone='Europe/Warsaw';
 $conn->set_charset("utf8");
 ?>
