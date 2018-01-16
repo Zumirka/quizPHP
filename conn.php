30b0e83a0dd93ef22ddb048ee18d6681d51bd7ed
Zumirka <dominika.bartek1207@gmail.com>
2018-01-16 20:37:02 +0100

    try change datetime

--- conn.php
+++ conn.php
@@ -5,5 +5,7 @@ $mysql_username = $url["user"];
 $mysql_password = $url["pass"];
 $server_name = $url["host"];
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $dba_name); 
+$mysql_qry = "set time_zone="Europe/Warsaw";"; 
+$conn->query($mysql_qry);
 $conn->set_charset("utf8");
 ?>
