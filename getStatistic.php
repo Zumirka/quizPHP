b13fa2f48d7dc0354de040cd26159a2a0e35ac0a
Zumirka <dominika.bartek1207@gmail.com>
2018-01-16 23:01:33 +0100

    t

--- getStatistic.php
+++ getStatistic.php
@@ -7,7 +7,7 @@ $mysql_qry = "select Id from user where Name='$user'";
 $result=$conn->query($mysql_qry);
 $row = mysqli_fetch_assoc($result);
 $user_id=$row["Id"];
-$mysql_qry="select* from statistic where UserId=;'$user_id'";
+$mysql_qry="select* from statistic where UserId='$user_id';";
 $result=$conn->query($mysql_qry);
 if($result)
 {
