89cb8dd5ebb8181d1232b4da45c988b0f8d1b475
Zumirka <dominika.bartek1207@gmail.com>
2018-01-19 19:18:27 +0100

    edit rand

--- getTest.php
+++ getTest.php
@@ -2,10 +2,14 @@
 require "conn.php";
 $id = $_POST["id"];
 $diff = $_POST["diff"];
-$mysql_qry = "SELECT q.Difficult,q.Content as Question,a.Content as Answer,a.IsTrue FROM (question q inner join answears a on a.QuestionId=q.Id) INNER JOIN(  
-SELECT DISTINCT q.Id FROM question q where q.CategoryId='$id' and q.Difficult='$diff' order by rand() limit 10) as q2 ON q2.Id=q.Id;"; 
-$result=$conn->query($mysql_qry);
 
+$mysql_qry="SELECT count(*)  FROM question q where q.CategoryId='$id' and q.Difficult='$diff';";
+$d = mysql_fetch_row($r);
+$rand = mt_rand(0,$d[0] - 1);
+
+$mysql_qry = "SELECT q.Difficult,q.Content as Question,a.Content as Answer,a.IsTrue FROM (question q inner join answears a on a.QuestionId=q.Id) INNER JOIN(  
+SELECT DISTINCT q.Id FROM question q where q.CategoryId='$id' and q.Difficult='$diff' limit $rand,10) as q2 ON q2.Id=q.Id;"; 
+$result=$conn->query($mysql_qry);
 if($result)
 {
 	while($row=mysqli_fetch_array($result))
@@ -18,3 +22,11 @@ if($result)
 }
 $conn->close();
 ?>
+
+$mysql_qry="SELECT count(*)  FROM question q where q.CategoryId='$id' and q.Difficult='$diff';";
+$d = mysql_fetch_row($r);
+$rand = mt_rand(0,$d[0] - 1);
+
+$mysql_qry = "SELECT q.Difficult,q.Content as Question,a.Content as Answer,a.IsTrue FROM (question q inner join answears a on a.QuestionId=q.Id) INNER JOIN(  
+SELECT DISTINCT q.Id FROM question q where q.CategoryId='$id' and q.Difficult='$diff' limit $rand,10) as q2 ON q2.Id=q.Id;"; 
+$result=$conn->query($mysql_qry);
