cc8d9378508e2752cedc8f081eaece7019f0a086
Zumirka <dominika.bartek1207@gmail.com>
2018-01-19 19:34:22 +0100

    rand work

--- getTest.php
+++ getTest.php
@@ -24,10 +24,3 @@ if($result)
 $conn->close();
 ?>
 
-$mysql_qry="SELECT count(*)  FROM question q where q.CategoryId='$id' and q.Difficult='$diff';";
-$d = mysql_fetch_row($r);
-$rand = mt_rand(0,$d[0] - 1);
-
-$mysql_qry = "SELECT q.Difficult,q.Content as Question,a.Content as Answer,a.IsTrue FROM (question q inner join answears a on a.QuestionId=q.Id) INNER JOIN(  
-SELECT DISTINCT q.Id FROM question q where q.CategoryId='$id' and q.Difficult='$diff' limit $rand,10) as q2 ON q2.Id=q.Id;"; 
-$result=$conn->query($mysql_qry);
