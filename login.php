ade0989b490e0cdb435a4e0b8dd51a1263089cd8
Zumirka <dominika.bartek1207@gmail.com>
2018-01-21 22:27:26 +0100

    clear code

--- login.php
+++ login.php
@@ -6,8 +6,6 @@ $mysql_qry = "select * from user where Name like '$user_name' and Password like 
 $result = mysqli_query( $conn,$mysql_qry); 
 if(mysqli_num_rows($result) > 0)
 {
-	$row=mysqli_fetch_assoc($result);
-	$name=$row["Name"];
 	echo "1";
 }
 else 
